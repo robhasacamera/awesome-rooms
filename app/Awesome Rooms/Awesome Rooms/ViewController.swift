9c510020220d76da87711253f3839c9c4167a5bb
Robert Cole <robhasacamera@gmail.com>
2018-09-18 12:18:19 -0500

    Created initial project.

---
+++ app/Awesome Rooms/Awesome Rooms/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Awesome Rooms
+//
+//  Created by Robert J Cole on 9/18/18.
+//  Copyright © 2018 Robert Cole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
