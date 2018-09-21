c9daa9f1bb890ecdbd8f5305a126c2142b760dab
Robert Cole <robhasacamera@gmail.com>
2018-09-21 16:26:09 -0500

    Adding more view classes.

---
+++ app/Awesome Rooms/Awesome Rooms/MeetingView.swift
@@ -0,0 +1,21 @@
+//
+//  MeetingView.swift
+//  Awesome Rooms
+//
+//  Created by Robert J Cole on 9/21/18.
+//  Copyright © 2018 Robert Cole. All rights reserved.
+//
+
+import UIKit
+
+class MeetingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
