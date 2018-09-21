e72b49b5176eb0fc5897bc032317e5a8165f6f6a
Robert Cole <robhasacamera@gmail.com>
2018-09-21 16:56:13 -0500

    Working on the storyboard connections and the room status view controller.

--- app/Awesome Rooms/Awesome Rooms/RoomStatusViewController.swift
+++ app/Awesome Rooms/Awesome Rooms/RoomStatusViewController.swift
@@ -9,21 +9,57 @@
 import UIKit
 
 class RoomStatusViewController: UIViewController, ReservationViewControllerDelegate {
-  @IBOutlet var quickBookView: QuickBookView!
-  
-  @IBOutlet weak var topStack: UIStackView!
-  
-  @IBOutlet weak var bottomStack: UIStackView!
-
+    
+    
+    
+    @IBOutlet weak var topStack: UIStackView!
+    
+    @IBOutlet weak var bottomStack: UIStackView!
+    
+    @IBOutlet var quickBookView: QuickBookView!
+    
+    @IBOutlet var topMessageView: MessageView!
+    
+    @IBOutlet var topMeetingView: MeetingView!
+    
+    @IBOutlet var bottomMessageView: MessageView!
+    
+    @IBOutlet var bottomMeetingView: MeetingView!
+    
     var eventClient: EventClient?
-
-  override func viewDidLoad() {
+    
+    let eventToCreate: Event?
+    
+    func createEvent(withLength: Int) -> Event {
+        let now = Date()
+        
+        let calendar = Calendar.current
+        
+        
+        
+    }
+    
+    @IBAction func book15MinuteMeeting(_ sender: Any) {
+        
+        
+        performSegue(withIdentifier: "BookMeetingSegue", sender: nil)
+    }
+    
+    @IBAction func book30MinuteMeeting(_ sender: Any) {
+        performSegue(withIdentifier: "BookMeetingSegue", sender: nil)
+    }
+    
+    @IBAction func book60MinuteMeeting(_ sender: Any) {
+        performSegue(withIdentifier: "BookMeetingSegue", sender: nil)
+    }
+    
+    override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         topStack.addArrangedSubview(quickBookView)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -35,24 +71,28 @@ class RoomStatusViewController: UIViewController, ReservationViewControllerDeleg
         }
         eventClient?.getEvents(completionHandler: didGetEvents, errorHandler: didNotGetEvents)
     }
-
+    
     func didGetEvents(_: [Event]) {
         // TODO parse array of events to display current or upcoming events
     }
-
+    
     func didNotGetEvents(message: String) {
         // Do nothing, just try again in one second
         return
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let controller = segue.destination as? ReservationViewController, let eventClient = eventClient {
+            controller?.setup(delegate: self, eventClient: eventClient, event: <#T##Event#>)
+        }
+        
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+ 
+    
 }
